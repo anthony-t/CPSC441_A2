2ddae55d32e5906614c1a8ee79a4d17314fbeccd
anthony-t <abtran@ucalgary.ca>
2018-02-21 14:45:57 -0700

    Removed the custom stream buffer

--- Octo.h
+++ Octo.h
@@ -5,10 +5,11 @@
 #define OCTOLEG_SIZE 1111
 #define OCTOBLOCK_SIZE OCTOLEGS_NUM * OCTOLEG_SIZE
 
+/*
 struct charbuffer : std::streambuf {
 	charbuffer(char * begin, char * end) {
 		this->setg(begin, begin, end);
 	}
-};
+};*/
 
 void getList(int n, std::vector<int> * v, int division, int max);
